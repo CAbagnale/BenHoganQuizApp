981894d800f52917dfc4fc72be7432f1923d7ad3
CAbagnale <connor.ashpole@outlook.com>
2019-07-09 21:00:16 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+let questionNumber = 0;
+let score = 0;
+
+function createQuestion () {
+    // probably need loop
+    // expect argument called question number 
+  console.log("createQuestion ran");
+  if (questionNumber < STORE.length) {
+    return `<div class="question-${questionNumber}">
+        <div class="question-number-display"><h2>Question #${questionNumber + 1}</h2></div>
+        <p>${STORE[questionNumber].question}</p>
+        <form>
+          <fieldset>
+            <label>
+              <input type="radio" value="${STORE[questionNumber].answers[0]}" name="answer" required="">
+              <span class="questionAnswer">${STORE[questionNumber].answers[0]}</span>
+            </label>
+            <label>
+              <input type="radio" value="${STORE[questionNumber].answers[1]}" name="answer" required="">
+              <span class="questionAnswer">${STORE[questionNumber].answers[1]}</span>
+            </label>
+            <label>
+              <input type="radio" value="${STORE[questionNumber].answers[2]}" name="answer" required="">
+              <span class="questionAnswer">${STORE[questionNumber].answers[2]}</span>
+            </label>
+            <label>
+              <input type="radio" value="${STORE[questionNumber].answers[3]}" name="answer" required="">
+              <span class="questionAnswer">${STORE[questionNumber].answers[3]}</span>
+            </label>
+          </fieldset>
+            <button type="submit" class="submitButton">Submit</button>
+        </form>
+      </div>`
+  }
+  else {
+    showResults();
+    restartQuiz();
+  }
+}
+
+function renderQuestion () {
+    $('.questionForm').html(createQuestion());
+    console.log("renderQuestion ran");    
+}
+
+function submitAnswer () {
+    // on form submit check selected answer against correct answer from STORE and
+    // run answerFeedbackCorrect/Incorrect
+    $('form').on('submit', function (event) {
+      console.log("submitAnswer ran");
+      event.preventDefault();
+      let selected = $('input:checked');
+      let userAnswer = selected.val();
+      let correctAnswer = `${STORE[questionNumber].correctAnswer}`;
+      if (userAnswer === correctAnswer) {
+        answerFeedbackCorrect();
+      }
+      else {
+        answerFeedbackIncorrect();
+      }
+    }
+    );
+    console.log("submitAnswer ran");
+}
+
+function answerFeedbackCorrect () {
+    console.log("answerFeedbackCorrect ran"); 
+    console.log(questionNumber);
+    if (questionNumber === 9) {
+      $('.questionForm').html(`<div><h2 class="response-title">Correct!</h2><p>${STORE[questionNumber].extraInfo}</p></div><button type="submit" class="finishButton">Finish</button>`);
+    }
+    else {
+      $('.questionForm').html(`<div><h2 class="response-title">Correct!</h2><p>${STORE[questionNumber].extraInfo}</p></div><button type="submit" class="nextButton">Next</button>`);
+    }
+    updateScore ();
+}
+
+function answerFeedbackIncorrect () {
+    // Let user know the answer is incorrect and display extra info
+    console.log("answerFeedbackIncorrect ran");
+    if (questionNumber === 9) {
+      $('.questionForm').html(`<div><h2>Incorrect.</h2><p>${STORE[questionNumber].extraInfo}</p></div><button type="submit" class="finishButton">Finish</button>`);
+    }
+    else {
+      $('.questionForm').html(`<div><h2>Incorrect.</h2><p>${STORE[questionNumber].extraInfo}</p></div><button type="submit" class="nextButton">Next</button>`);
+    } 
+}
+
+function updateScore () {
+  score ++;
+  $('.score').text(score);
+  console.log("updateScore ran");
+}
+
+function nextQuestion () {
+  $('.questionForm').on('click', '.nextButton', function (event) {
+    console.log("nextQuestion ran");
+    questionNumber ++;
+    $('.questionNumber').text(questionNumber + 1);
+    renderQuestion ();
+    submitAnswer ();
+  })
+  
+}
+
+function lastQuestion () {
+  $('.questionForm').on('click', '.finishButton', function (event) {
+    console.log("lastQuestion ran");
+    questionNumber ++;
+    renderQuestion ();
+    submitAnswer ();
+  })
+  
+}
+
+function showResults () {
+
+  if (score === 10) {
+    $('.questionForm').html(`<div class="resultsPerfect"><h1 class="results-title">Perfection!</h1><h2>You scored ${score}/10</h2><p>"There are no shortcuts in the quest for perfection." - Hogan</p><button class="restartButton">Restart</button></div>`);
+    console.log("showResults ran");
+  }
+  else if (score > 6) {
+    $('.questionForm').html(`<div class="resultsOkay"><h1 class="results-title">Not bad.</h1><h2>You scored ${score}/10</h2><p>"Golf is not a game of good shots. It's a game of bad shots." - Hogan</p><button class="restartButton">Restart</button></div>`);
+    console.log("showResults ran");
+  }
+  else {
+    $('.questionForm').html(`<div class="resultsBad"><h1 class="results-title">Ouch.</h1><h2>You scored ${score}/10</h2><p>"Shoot a lower score than everybody else." - Hogan</p><p>Wait, that might not apply here...</p><button class="restartButton">Restart</button></div>`);
+    console.log("showResults ran");
+  }
+}
+
+function restartQuiz () {
+  $('main').on('click', '.restartButton', function (event) {
+    location.reload();
+  });
+}
+function startQuiz () {
+    $('.quizStart').on('click', '.startButton', function (event) {
+      console.log("startQuiz ran");
+      $('.quizStart').remove();
+      $('.questionForm').css('display', 'block');
+      $('.scoreBanner').css('display', 'block');
+      $('.questionNumber').text(1);
+    renderQuestion ();
+    submitAnswer ();
+    nextQuestion ();
+    lastQuestion ();
+  });
+  }
+
+$(startQuiz);
---
+++ store.js
@@ -0,0 +1,112 @@
+const STORE = [
+    {
+        question: 'Which of the following nicknames was Ben Hogan NOT known as?',
+        answers: [
+            'The Hawk',
+            'Bantam Ben',
+		    'Big Ben',
+		    'The Wee Iceman',
+        ],
+        correctAnswer: 'Big Ben',
+        extraInfo: 'Hogan was never known as big ben.',
+    },
+    {
+        question: 'Ben Hogan served in which branch of the US Military?',
+        answers: [
+            'Army',
+            'Navy',
+		    'Air Force',
+		    'Coast Guard',
+        ],
+        correctAnswer: 'Air Force',
+        extraInfo: 'Ben Hogan served in the Air Force',
+    },
+    {
+        question: 'In 1950, Hogan famously used what uncommon club to win the U.S Open Championship during the 18 hole playoff?',
+        answers: [
+            'a rescue wood',
+            'a driver from the fairway',
+		    '1 iron',
+		    '2 iron',
+        ],
+        correctAnswer: '1 iron',
+        extraInfo: 'Hogan famously used his 1 iron in the playoff. There is a plaque where the shot took place 200 yards from the green.',
+    },
+    {
+        question: 'Ben Hogan is known by amateur golfers and professionals alike as the greatest ever at what aspect of the game of golf?',
+        answers: [
+            'Ball-striking',
+            'Putting',
+		    'Teaching',
+		    'Rescue shots',
+        ],
+        correctAnswer: 'Ball-striking',
+        extraInfo: 'His ball-striking abilities were second-to-none.',
+    },
+    {
+        question: 'How many Major Championships did Ben Hogan win?',
+        answers: [
+            '11',
+            '9',
+		    '7',
+		    '5',
+        ],
+        correctAnswer: '9',
+        extraInfo: 'Hogan won a total of 9 Major Championships',
+    },
+    {
+        question: `What was the title of Ben Hogan's instructional book?`,
+        answers: [
+            'The Four Lessons',
+            'The Five Lessons',
+		    'The Six Lessons',
+		    'The Seven Lessons',
+        ],
+        correctAnswer: 'The Five Lessons',
+        extraInfo: `His instructional book on how to best emulate his own swing was entited, 'The Five Lessons'`,
+    },
+    {
+        question: 'Of the following, which is not a major section in his instructional book?',
+        answers: [
+            'The fundamentals',
+            'Preparation',
+		    'Stance and Posture',
+		    'The swing',
+        ],
+        correctAnswer: 'Preparation',
+        extraInfo: `'Preparation' was not a part of his instructional book.`,
+    },
+    {
+        question: `What was the title of the biographical film detailed Ben's life starring Glen Ford?`,
+        answers: [
+            'Hogan',
+            'The Masters',
+		    'Hickok Belt',
+		    'Follow the Sun',
+        ],
+        correctAnswer: 'Follow the Sun',
+        extraInfo: `'Follow the Sun' is the title of the biopic about Hogan's life.`,
+    },
+    {
+        question: 'Hogan graduated from what high school?',
+        answers: [
+            'E.L. Paschal High School',
+            'R.L. Paschal High School',
+		    'P.L. Paschal High School',
+		    `He didn't graduate from high school.`,
+        ],
+        correctAnswer:  `He didn't graduate from high school.`,
+        extraInfo: 'Hogan dropped out of R.L. Paschal High School in his final semester',
+    },
+    {
+        question: 'Ben Hogan and who else were the only two golfers to ever receive a ticker-tape parade?',
+        answers: [
+            'Gene Sarazen',
+            'Tiger Woods',
+		    'Phil Mickelson',
+		    'Sergio Garcia',
+        ],
+        correctAnswer: 'Gene Sarazen',
+        extraInfo: 'Hogan received a ticker-tape parade upon his return from England after winning the British Open. Gene Sarazen was the only other golfer to receive this honor.',
+    },
+]
